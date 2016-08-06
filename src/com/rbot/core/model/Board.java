ef32f91ff93e8c934a7b267f135442f4b7814464
tacoder <abhinav.singh21093@gmail.com>
2016-08-06 17:05:37 +0530

    Added coordinates

--- src/com/rbot/core/model/Board.java
+++ src/com/rbot/core/model/Board.java
@@ -1,5 +1,7 @@
 package com.rbot.core.model;
 
+import java.util.List;
+
 public class Board {
 
 	public static final Integer boardSize = 8;
@@ -32,5 +34,40 @@ public class Board {
 		}
 	}
 	
-//	public move(CellType toInsert, )
+	
+	public void move(CellType toInsert, Coordinates insertAt) throws Exception{
+		if (!isValidMove(toInsert,insertAt)){
+			throw new Exception("Invalid Move!");
+		} else {
+			List<Coordinates> movesToMake = getCoordinatesForMoveAt(toInsert,insertAt);
+			for(Coordinates coordinates : movesToMake){
+				reversi(toInsert,insertAt,coordinates);
+			}
+		}
+	}
+
+	private void reversi(CellType toInsert, Coordinates insertAt, Coordinates coordinates) {
+		List<Coordinates> cells = getAllCellCoordinatesBetween(insertAt, coordinates);
+		for(Coordinates cell : cells){
+			paintCell(cell,toInsert);
+		}
+	}
+
+	private void paintCell(Coordinates cell, CellType toInsert) {
+		grid[cell.getRow()][cell.getColumn()].setContent(toInsert);
+	}
+
+	private List<Coordinates> getAllCellCoordinatesBetween(Coordinates insertAt, Coordinates coordinates) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List<Coordinates> getCoordinatesForMoveAt(CellType toInsert, Coordinates insertAt) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Boolean isValidMove(CellType toInsert, Coordinates insertAt) {
+		return Boolean.TRUE;
+	}
 }
---
+++ src/com/rbot/core/model/Coordinates.java
@@ -0,0 +1,23 @@
+package com.rbot.core.model;
+
+public class Coordinates {
+	private Integer row;
+	private Integer column;
+	public Integer getColumn() {
+		return column;
+	}
+	public void setColumn(Integer column) {
+		this.column = column;
+	}
+	public Integer getRow() {
+		return row;
+	}
+	public void setRow(Integer row) {
+		this.row = row;
+	}
+	public Coordinates(Integer row, Integer column) {
+		super();
+		this.row = row;
+		this.column = column;
+	}
+}
