0d176f15a09c072562c02babbbc321837bdb0dd3
tacoder <abhinav.singh21093@gmail.com>
2016-08-31 21:41:54 +0530

    initial bot commit

---
+++ Bot/src/com/ai/bot/Bot.java
@@ -0,0 +1,14 @@
+package com.ai.bot;
+
+import java.util.ArrayList;
+
+import com.game.wrapper.interfaces.GenericGame;
+import com.game.wrapper.interfaces.GenericMove;
+
+public class Bot {
+	public static void getBestMove(GenericGame game){
+		GenericMove bestMove = null;
+		ArrayList<GenericGame> bfs = new ArrayList<>();
+		
+	}
+}
